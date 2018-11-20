a2e9003182103eb3d95d0fd535c0a9aa5e0da371
vellerzheng <vellerzheng2012@163.com>
2018-11-20 16:41:09 +0800

    static resource

--- storage-web/src/main/resources/static/js/main.js
+++ storage-web/src/main/resources/static/js/main.js
@@ -29,7 +29,7 @@ function main() {
 
     
  $(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
